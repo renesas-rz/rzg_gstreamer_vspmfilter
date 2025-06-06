3bad93c42be267f909cf2b98bf8b18ebe48a764f
Kiet Pham <kiet.pham.xb@renesas.com>
2025-06-06 16:59:43 +0700

    Correct alignment from downstream before rounding up stride
    We misunderstood about stride-align in GstVideoAlignment structure. It should
    be 2^n - 1 instead of 2^n. If downstream proposes an odd alignment, stride
    could be calculated incorrectly.
    
    We must round up the alignment to avoid this issue and for backward compatible
    as well.
    
    Signed-off-by: Kiet Pham <kiet.pham.xb@renesas.com>

--- gstvspmfilter.c
+++ gstvspmfilter.c
@@ -498,7 +498,13 @@ gst_vspm_filter_set_buffer_info (GstVspmFilter * space,
     /* If we have alignment requirement from downstream */
     if (align) {
       /* FIXME: Currently, we ignore padding and only update stride */
-      stride = GST_ROUND_UP_N(stride, align->stride_align[i]);
+
+      /* According to the implementation of Gstreamer, stride_align, logically,
+       * must be a number equal to 2^N-1 instead of 2^N. Downstream proposes
+       * alignment as 2^N in the older versions and 2^N-1 in the new version.
+       * So, we should round the alignment up before using to get the same
+       * result for both cases */
+      stride = GST_ROUND_UP_N(stride, GST_ROUND_UP_2(align->stride_align[i]));
     } else {
       GST_DEBUG_OBJECT (space, "No stride alignment requirement from downstream");
     }
